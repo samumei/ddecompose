eafaf75a1300b874476f6e9d1df478da11871985
David Gallusser <70625545+davidgallusser@users.noreply.github.com>
2023-05-25 13:43:11 +0200

    vcov adjustment

--- R/ob_deco.R
+++ R/ob_deco.R
@@ -460,7 +460,7 @@ ob_deco_calculate_vcov  <- function(beta0,
   Cov_observed_diff <-  Cov_Xb1 + Cov_Xb0
 
   if(reference_0){
-    Cov_XbC <- Cov_X1 * Cov_beta0 + Cov_beta0 * (X1 %*% t(X1)) + Cov_X1 *  (beta0 %*% t(beta0))
+    Cov_XbC <- Cov_X1 * Cov_beta0 + Cov_beta0 * (X1 %*% t(X1)) + Cov_X1 * (beta0 %*% t(beta0))
     Cov_structure_effect <-  Cov_Xb1 + Cov_XbC
     Cov_composition_effect <-  Cov_Xb0 + Cov_XbC
   }else{
