ac57a3a588b57021d95c9c5b91ab5163f116a22d
Samuel Jann Meier <50319350+samumei@users.noreply.github.com>
2023-06-26 16:59:03 +0200

    Update test-ob_deco.R
    Added validation test from decomposition lecture exercises

--- tests/testthat/test-ob_deco.R
+++ tests/testthat/test-ob_deco.R
@@ -66,15 +66,15 @@
 #
 #   ## same decomposition results
 #   # overall
-#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Composition_effect[1] * -1,
+#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Composition_effect[1],
 #                          unname(oaxaca_results$twofold$overall[2,2]))
-#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Structure_effect[1] * -1,
+#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Structure_effect[1],
 #                          unname(oaxaca_results$twofold$overall[2,4]))
 #
 #   # detailed
-#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Composition_effect[2:4] * -1,
+#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Composition_effect[2:4],
 #                          unname(oaxaca_results$twofold$variables[[2]][,2]))
-#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Structure_effect[2:4] * -1,
+#   testthat::expect_equal(ddeco_results$ob_deco$decomposition_terms$Structure_effect[2:4],
 #                          unname(oaxaca_results$twofold$variables[[2]][,4]))
 #
 #   # SE similar
@@ -97,17 +97,17 @@
 #                                          data = chicago,
 #                                          group = foreign.born)
 #
-#   testthat::expect_equal(ddeco_results_analytical_se$decomposition_vcov$decomposition_terms_se$Composition_effect[1],
+#   testthat::expect_equal(ddeco_results_analytical_se$ob_deco$decomposition_vcov$decomposition_terms_se$Composition_effect[1],
 #                          unname(oaxaca_results$twofold$overall[2,3]),
 #                          tolerance = 0.03)
-#   testthat::expect_equal(ddeco_results_analytical_se$decomposition_vcov$decomposition_terms_se$Structure_effect[1],
+#   testthat::expect_equal(ddeco_results_analytical_se$ob_deco$decomposition_vcov$decomposition_terms_se$Structure_effect[1],
 #                          unname(oaxaca_results$twofold$overall[2,5]),
 #                          tolerance = 0.1)
 #
-#   testthat::expect_equal(ddeco_results_analytical_se$decomposition_vcov$decomposition_terms_se$Composition_effect[2:4],
+#   testthat::expect_equal(ddeco_results_analytical_se$ob_deco$decomposition_vcov$decomposition_terms_se$Composition_effect[2:4],
 #                          unname(oaxaca_results$twofold$variables[[2]][,3]),
 #                          tolerance = 0.03)
-#   testthat::expect_equal(ddeco_results_analytical_se$decomposition_vcov$decomposition_terms_se$Structure_effect[2:4],
+#   testthat::expect_equal(ddeco_results_analytical_se$ob_deco$decomposition_vcov$decomposition_terms_se$Structure_effect[2:4],
 #                          unname(oaxaca_results$twofold$variables[[2]][,5]),
 #                          tolerance = 0.15)
 # })
@@ -158,10 +158,8 @@
 #
 #   # check that means are the same
 #
-#   # YET TO IMPLEMENT in ob_deco
-#
 #   # check that overall diff is as expected
-#   overall_diff <- mean_female[1] - mean_male[1]
+#   overall_diff <- mean_male[1] - mean_female[1]
 #
 #   female_observed <- sum(mean_female[2:4] * reg_female$coefficients[2:4],
 #                          reg_female$coefficients[1])
@@ -170,7 +168,7 @@
 #   male_observed <- sum(mean_male[2:4] * reg_male$coefficients[2:4],
 #                          reg_male$coefficients[1])
 #   testthat::expect_equal(male_observed, mean_male[[1]])
-#   testthat::expect_equal(overall_diff[[1]], female_observed - male_observed)
+#   testthat::expect_equal(overall_diff[[1]],male_observed -  female_observed)
 #   testthat::expect_equal(overall_diff[[1]], deco_results$ob_deco$decomposition_terms$Observed_difference[1])
 #
 #
@@ -178,21 +176,13 @@
 #   female_counterfactual <- sum(mean_female[2:4] * reg_male$coefficients[2:4],
 #                              reg_male$coefficients[1])
 #
-#   unexplained <- female_observed - female_counterfactual
-#   explained <- female_counterfactual - male_observed
+#   unexplained <- female_counterfactual - female_observed
+#   explained <- male_observed -female_counterfactual
 #
 #   testthat::expect_equal(deco_results$ob_deco$decomposition_terms$Composition_effect[1],
 #                          explained)
 #   testthat::expect_equal(deco_results$ob_deco$decomposition_terms$Structure_effect[1],
 #                          unexplained)
-#   testthat::expect_equal(deco_results$ob_deco$decomposition_terms$Structure_effect[2:5],
-#                          unname(c(reg_female$coefficients[1] - reg_male$coefficients[1],
-#                              (mean_female[2:4] * reg_female$coefficients[2:4]) -
-#                                (mean_female[2:4] * reg_male$coefficients[2:4]))))
-#
-#   testthat::expect_equal(deco_results$ob_deco$decomposition_terms$Composition_effect[2:5],
-#                          unname(c(0, (mean_female[2:4] * reg_male$coefficients[2:4]) -
-#                                     (mean_male[2:4] * reg_male$coefficients[2:4]))))
 # })
 #
 #
@@ -379,7 +369,6 @@
 #
 # })
 #
-#
 # test_that("rifreg decomposition does not throw an error with bootstrap", {
 #
 #   set.seed(43825081)
@@ -477,4 +466,257 @@
 #                          tolerance = 0.0075)
 # })
 
-
+# test_that("ob_deco() replicates Table 3, p. 41, in FLF 2011 Handbook Chapter", {
+#
+#   data("nlys00")
+#   mod1 <- log(wage) ~ age + central_city + msa + region + black +
+#       hispanic + education + afqt + family_responsibility + years_worked_civilian +
+#       years_worked_military + part_time + industry
+#
+#     # Using female coefficients (reference_0 = TRUE) to estimate counterfactual mean
+#     deco_female_as_reference <- ob_deco(formula = mod1,
+#                                         data = nlys00,
+#                                         group = female,
+#                                         reference_0 = TRUE)
+#   testthat::expect_equal(deco_female_as_reference$ob_deco$decomposition_terms$Observed_difference[1], 0.233, tolerance = 0.01)
+#    testthat::expect_equal(deco_female_as_reference$ob_deco$decomposition_terms$Composition_effect[1], 0.136, tolerance = 0.01)
+#    testthat::expect_equal(deco_female_as_reference$ob_deco$decomposition_terms$Structure_effect[1], 0.097, tolerance = 0.01)
+#
+#    deco_male_as_reference <- ob_deco(formula = mod1,
+#                                        data = nlys00,
+#                                        group = female,
+#                                        reference_0 = FALSE)
+#    testthat::expect_equal(deco_male_as_reference$ob_deco$decomposition_terms$Observed_difference[1], 0.233, tolerance = 0.01)
+#    testthat::expect_equal(deco_male_as_reference$ob_deco$decomposition_terms$Composition_effect[1], 0.197, tolerance = 0.01)
+#    testthat::expect_equal(deco_male_as_reference$ob_deco$decomposition_terms$Structure_effect[1], 0.036, tolerance = 0.01)
+#
+#
+# })
+# test_that("ob_deco() replicates Table 4-C, p. 62, in FLF 2011 Handbook Chapter", {
+#   browser()
+#
+# #   Abweichungen: sind sie wirklich grösser als beim OLS?? -> ja, schon..
+# #   NORMALizarione anschauen --> erst beim detaillierten Zeugs.
+# #   Gewichte anschauen
+# #   Ihre Berechnungen in Stata-File anschauen
+# #   Umgewichtung haben sie starke umgewichtungen.
+# #   RIFREG umgewichtung eigene Funktion -> wie bei DFL-Deco mit mehreren bedingungen.-> schauen, was du noch machen mussen
+#
+#
+#   data("nlys00")
+#   mod1 <- log(wage) ~ age + central_city + msa + region + black +
+#     hispanic + education + afqt + family_responsibility + years_worked_civilian +
+#     years_worked_military + part_time + industry
+#
+#   # Using male coefficients to estimate counterfactual mean
+#   rifreg_deco <- ob_deco(formula = mod1,
+#                                       data = nlys00,
+#                                       group = female,
+#                                       rifreg = TRUE,
+#                                       rifreg_statistic = "quantiles",
+#                                       rifreg_probs = 0.5, # c(0.1, 0.5, 0.9),
+#                                       reference_0 = FALSE)
+#
+#   rifreg_deco_female <- ob_deco(formula = mod1,
+#                          data = nlys00,
+#                          group = female,
+#                          rifreg = TRUE,
+#                          rifreg_statistic = "quantiles",
+#                          rifreg_probs = 0.5, #c(0.1, 0.5, 0.9),
+#                          reference_0 = TRUE)
+#   rifreg_deco$quantile_0.5$decomposition_terms$Observed_difference[1]
+#
+#   testthat::expect_equal(rifreg_deco$quantile_0.1$decomposition_terms$Observed_difference[1], 0.180, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.1$decomposition_terms$Composition_effect[1], 0.274, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.1$decomposition_terms$Structure_effect[1], -0.094, tolerance = 0.01)
+#
+#   testthat::expect_equal(rifreg_deco$quantile_0.5$decomposition_terms$Observed_difference[1], 0.241, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.5$decomposition_terms$Composition_effect[1], 0.208, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.5$decomposition_terms$Structure_effect[1], 0.033, tolerance = 0.01)
+#
+#   testthat::expect_equal(rifreg_deco$quantile_0.9$decomposition_terms$Observed_difference[1], 0.260, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.9$decomposition_terms$Composition_effect[1], 0.136, tolerance = 0.01)
+#   testthat::expect_equal(rifreg_deco$quantile_0.9$decomposition_terms$Structure_effect[1], 0.124, tolerance = 0.01)
+#
+#
+#   browser()
+#   # Replicate statistics in table 5-D, p.67, in in FLF (2011)
+#   flf_variance  <- ob_deco(flf_model,
+#                            data = men8305_full,
+#                            weights = weights,
+#                            group = year,
+#                            reference_0 = TRUE,
+#                            rifreg = TRUE,
+#                            rifreg_statistic = "variance")
+#   View(flf_variance[["variance"]][["decomposition_terms"]][1,])
+#
+#   flf_gini  <- ob_deco(flf_model,
+#                        data = men8305_full,
+#                        weights = weights,
+#                        group = year,
+#                        reference_0 = TRUE,
+#                        rifreg = TRUE,
+#                        rifreg_statistic = "gini")
+#   View(flf_gini[["gini"]][["decomposition_terms"]][1,])
+#
+#
+#   flf_iq_range_p90_p10  <- ob_deco(flf_model,
+#                                    data = men8305_full,
+#                                    weights = weights,
+#                                    group = year,
+#                                    reference_0 = TRUE,
+#                                    rifreg = TRUE,
+#                                    rifreg_statistic = "interquantile_range",
+#                                    rifreg_probs = c(0.9, 0.1))
+#   View(flf_iq_range_p90_p10[["interquantile_range"]][["decomposition_terms"]][1,])
+#
+#   rifreg_results <- rbind(flf_variance[["ob_deco"]][["decomposition_terms"]][1,],
+#                           flf_gini[["ob_deco"]][["decomposition_terms"]][1,],
+#                           flf_iq_range_p90_p10[["ob_deco"]][["decomposition_terms"]][1,])
+#   results_rifreg_deco <- flf_male_inequality_table_5$decomposition_terms
+#
+#   published_results_FLF_table_5 <- data.frame(statistic = results_dfl_deco$statistic,
+#                                               `Observed difference` = c(0.0617, 0.1091, 0.1827, -0.0736),
+#                                               `Structure effect` = c(0.0408, 0.0336, 0.1637, -0.1301),
+#                                               `Composition effect` = c(0.0208, 0.0756, 0.0191, 0.0565))
+#   rownames(results_dfl_deco) <- rownames(published_results_FLF_table_5) <- 1:4
+#   names(published_results_FLF_table_5) <- names(results_dfl_deco)
+#   testthat::expect_equal(results_dfl_deco,
+#                          published_results_FLF_table_5,
+#                          tolerance = 0.0075)
+# })
+# test_that("dfl_deco() replicates Table 5-D, p. 67, in FLF 2011 Handbook Chapter", {
+#
+#   #load("data-raw/men8305_full.rda")
+#   men8305_full$weights <- men8305_full$weights/sum(men8305_full$weights) * length(men8305_full$weights)
+#   flf_model <- log(wage) ~ union*(education + experience) + education*experience
+#
+#   browser()
+#   # Replicate statistics in table 5-D, p.67, in in FLF (2011)
+#   flf_variance  <- ob_deco(flf_model,
+#                            data = men8305_full,
+#                            weights = weights,
+#                            group = year,
+#                            reference_0 = FALSE,
+#                            rifreg = TRUE,
+#                            rifreg_statistic = "variance")
+#   View(flf_variance[["variance"]][["decomposition_terms"]][1,])
+#
+#   flf_gini  <- ob_deco(flf_model,
+#                        data = men8305_full,
+#                        weights = weights,
+#                        group = year,
+#                        reference_0 = TRUE,
+#                        rifreg = TRUE,
+#                        rifreg_statistic = "gini")
+#   View(flf_gini[["gini"]][["decomposition_terms"]][1,])
+#
+#
+#   flf_iq_range_p90_p10  <- ob_deco(flf_model,
+#                                    data = men8305_full,
+#                                    weights = weights,
+#                                    group = year,
+#                                    reference_0 = TRUE,
+#                                    rifreg = TRUE,
+#                                    rifreg_statistic = "interquantile_range",
+#                                    rifreg_probs = c(0.9, 0.1))
+#   View(flf_iq_range_p90_p10[["interquantile_range"]][["decomposition_terms"]][1,])
+#
+#   rifreg_results <- rbind(flf_variance[["ob_deco"]][["decomposition_terms"]][1,],
+#                           flf_gini[["ob_deco"]][["decomposition_terms"]][1,],
+#                           flf_iq_range_p90_p10[["ob_deco"]][["decomposition_terms"]][1,])
+#   results_rifreg_deco <- flf_male_inequality_table_5$decomposition_terms
+#
+#   published_results_FLF_table_5 <- data.frame(statistic = results_dfl_deco$statistic,
+#                                               `Observed difference` = c(0.0617, 0.1091, 0.1827, -0.0736),
+#                                               `Structure effect` = c(0.0408, 0.0336, 0.1637, -0.1301),
+#                                               `Composition effect` = c(0.0208, 0.0756, 0.0191, 0.0565))
+#   rownames(results_dfl_deco) <- rownames(published_results_FLF_table_5) <- 1:4
+#   names(published_results_FLF_table_5) <- names(results_dfl_deco)
+#   testthat::expect_equal(results_dfl_deco,
+#                          published_results_FLF_table_5,
+#                          tolerance = 0.0075)
+# })
+#
+#
+# test_that("same results as in lecture exercises", {
+#
+#   # get and prepare data
+#   #gsoep29 <- readstata13::read.dta13("data-raw/gsoep29.dta")
+#   total_n <- nrow(gsoep29)
+#   gsoep29$age <- 2012 - gsoep29$bcgeburt
+#   gsoep29 <- subset(gsoep29, age >=25 & age <= 55)
+#   total_n - nrow(gsoep29)
+#
+#   gsoep29$wage <- with(gsoep29, ifelse(labgro12 > 0 & bctatzeit > 0, labgro12 / (bctatzeit * 4.3), NA))
+#   gsoep29$lnwage <- log(gsoep29$wage)
+#
+#   gsoep29$schooling <- with(gsoep29, ifelse(bcbilzeit > 0, bcbilzeit, NA))
+#   gsoep29$ft_experience <- with(gsoep29, ifelse(expft12 >= 0, expft12, NA))
+#   gsoep29$ft_experience2 <- with(gsoep29, ifelse(expft12 >= 0, expft12^2, NA))
+#
+#   summary(gsoep29[c("bcsex", "wage", "lnwage", "schooling", "ft_experience", "ft_experience2")])
+#
+#   gsoep29$tenure <- with(gsoep29, ifelse(bcerwzeit >= 0, bcerwzeit, NA))
+#   gsoep29$ISEI <- with(gsoep29, ifelse(isei12 > 0, isei12, NA))
+#
+#   gsoep29 <- na.omit(gsoep29[, c("bcsex", "lnwage", "schooling", "ft_experience", "ft_experience2", "tenure", "ISEI", "bcphrf")])
+#   browser()
+#
+#   # without weights
+#   ob_deco_results <- ob_deco(formula = lnwage ~ schooling + ft_experience + ft_experience2,
+#                              data = gsoep29,
+#                              group = bcsex,
+#                              weights = bcphrf,
+#                              reference_0 = TRUE)
+#
+#   browser()
+#
+#   testthat::expect_equal(ob_deco_results$ob_deco$decomposition_terms$Observed_difference[1],
+#                          0.250,
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(ob_deco_results$ob_deco$decomposition_terms$Composition_effect[c(1, 3)],
+#                          c(0.149, -0.00630),
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(sum(ob_deco_results$ob_deco$decomposition_terms$Composition_effect[4:5]),
+#                          0.155,
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(ob_deco_results$ob_deco$decomposition_terms$Structure_effect[1:3],
+#                          c(0.101, -0.121, 0.0856),
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(sum(ob_deco_results$ob_deco$decomposition_terms$Structure_effect[4:5]),
+#                          0.136,
+#                          tolerance = 0.0075)
+#   browser()
+#   # with weights
+#   ob_deco_results_weights <- ob_deco(formula = lnwage ~ schooling + ft_experience + ft_experience2,
+#                                      data = gsoep29,
+#                                      group = bcsex,
+#                                      weights = bcphrf,
+#                                      reference_0 = TRUE)
+#
+#   testthat::expect_equal(ob_deco_results_weights$ob_deco$decomposition_terms$Observed_difference[1],
+#                          0.223,
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(ob_deco_results_weights$ob_deco$decomposition_terms$Composition_effect[c(1, 3)],
+#                          c(0.114, -0.0162),
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(sum(ob_deco_results_weights$ob_deco$decomposition_terms$Composition_effect[4:5]),
+#                          0.130,
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(ob_deco_results_weights$ob_deco$decomposition_terms$Structure_effect[1:3],
+#                          c(0.109, -0.126, 0.117),
+#                          tolerance = 0.0075)
+#
+#   testthat::expect_equal(sum(ob_deco_results_weights$ob_deco$decomposition_terms$Structure_effect[4:5]),
+#                          0.119,
+#                          tolerance = 0.0075)
+#
+# })
